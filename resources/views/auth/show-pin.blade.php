edb90c3123b722be59c5011d6efa11770085cae7
leo <leo.studenikin@gmail.com>
2021-11-10 10:42:40 +0300

    removed anchor id from pin page

--- resources/views/auth/show-pin.blade.php
+++ resources/views/auth/show-pin.blade.php
@@ -2,7 +2,6 @@
 @section('main-content')
 
 <div class="m-auto w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg text-center">
-    <h2 class="mb-6 font-extrabold text-gray-900">anchor_id : {{$anchor_id}}</h2>
     <h2 class="mb-6 font-extrabold text-gray-900">Safe Username: {{$safe_id}}</h2>
     <p>A notification has been sent to your mobile device for authentication.</p>
     <p class="mt-2 text-red-400">If you did not receive a notification, please check that notifications are turned ‘on’ for The SAFE Button app.</p>
